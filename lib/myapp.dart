f4533b378bd7a2f5b9d284b5e963b44b6acf679c
User-3397 <user3396.3301@gmail.com>
2025-12-02 01:05:06 +0000

    Adicionando arquivos iniciais

--- lib/myapp.dart
+++ lib/myapp.dart
@@ -1,19 +1,48 @@
+//import 'dart:html';
+//import 'dart:io';
+
 import 'package:flutter/material.dart';
-import 'cadastrarpessoa.dart';
-import 'confirma_cadastro.dart';
-import 'detalhes_aniversario.dart';
-import 'listaraniversarios.dart';
-import 'myhomepage.dart';
+import '/views/cadastrarpessoa.dart';
+import '/views/confirma_cadastro.dart';
+import '/views/detalhes_aniversario.dart';
+import '/views/listaraniversarios.dart';
+import '/views/myhomepage.dart';
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Agenda de Aniversários',
-      theme: ThemeData(primarySwatch: Colors.blue),
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        //primaryColor: Colors.red, // Cor principal
+        //primarySwatch: Color(0xff283846),
+        scaffoldBackgroundColor: Color(0xff2b2b2b), // Fundo padrão
+
+        textTheme: const TextTheme(
+
+            //bodySmall: TextStyle(fontSize: 25, color: Colors.yellow),
+            //bodyLarge: TextStyle(fontSize: 18, color: Color(0xffdef8df)),
+            //bodyMedium: TextStyle(fontSize: 16, color: Color(0xddb06969)),
+            //titleLarge: TextStyle(fontWeight: FontWeight.bold),
+            //titleSmall: TextStyle(fontSize: 25, color: Colors.yellow),
+            ),
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Color(0xff585858),
+          foregroundColor: Colors.white,
+          elevation: 4,
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: Colors.amber,
+            foregroundColor: Colors.black,
+            textStyle: TextStyle(fontSize: 16),
+          ),
+        ),
+      ),
       // SAi o home e entram as rotas de navegação
       initialRoute: MyHomePage.nomeRota,
       routes: {
